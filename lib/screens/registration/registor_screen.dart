76067b588d2ffc4a3b160f91715944452f97c951
nodir <nodirbarotov0707@gmail.com>
2022-03-30 09:11:05 +0500

    just do something

--- lib/screens/registration/registor_screen.dart
+++ lib/screens/registration/registor_screen.dart
@@ -9,52 +9,150 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  int _index = 0;
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.arrow_back_ios),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: const Icon(
+            Icons.arrow_back_ios,
+            color: Color(0xFF19AF8B),
           ),
-          body: Stepper(
-            type: StepperType.horizontal,
-            currentStep: _index,
-            onStepCancel: () {
-              if (_index > 0) {
-                setState(() {
-                  _index -= 1;
-                });
-              }
-            },
-            onStepContinue: () {
-              if (_index <= 0) {
-                setState(() {
-                  _index += 1;
-                });
-              }
-            },
-            onStepTapped: (int index) {
-              setState(() {
-                _index = index;
-              });
-            },
-            steps: <Step>[
-              Step(
-                title: Divider(),
-                content: Container(alignment: Alignment.centerLeft, child: const Text('Content for Step 1')),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            Container(
+                              height: 30,
+                              width: 30,
+                              decoration: const BoxDecoration(
+                                color: Color(0xFF19AF8B),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            const Text(
+                              '1',
+                              style: TextStyle(fontSize: 16, color: Color(0xFFE1F1ED)),
+                            )
+                          ],
+                        ),
+                        const Expanded(
+                            child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Divider(
+                            thickness: 3,
+                            height: 16,
+                            color: Color(0xFF19AF8B),
+                          ),
+                        )),
+                        Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            Container(
+                              height: 30,
+                              width: 30,
+                              decoration: const BoxDecoration(
+                                color: Color(0xFFE1F1ED),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            const Text(
+                              '2',
+                              style: TextStyle(fontSize: 16, color: Color(0xFF19AF8B)),
+                            )
+                          ],
+                        ),
+                        const Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Divider(
+                              thickness: 3,
+                              height: 16,
+                              color: Color(0xFFE1F1ED),
+                            ),
+                          ),
+                        ),
+                        Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: [
+                            Container(
+                              height: 30,
+                              width: 30,
+                              decoration: new BoxDecoration(
+                                color: const Color(0xFFE1F1ED),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            const Text(
+                              '3',
+                              style: TextStyle(fontSize: 16, color: Color(0xFF19AF8B)),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 27),
+                      child: const Text(
+                        'Создайте парол',
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Color(0xFF1D2927)),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Этот пароль нужен для разблокировки вашего кошелька Green Monet только \n на этом устройстве',
+                          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Color(0xFF4C5F5A)), textAlign: TextAlign.center),
+                    ),
+                  ],
+                ),
               ),
-              const Step(
-                title: Divider(),
-                content: Text('Content for Step 2'),
-              ),
-              const Step(
-                title: Divider(),
-                content: Text('Content for Step 2'),
-              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: const [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Новый парол',
+                        style: TextStyle(
+                          color: Color(0xFF1D2927),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          iconColor: Color(0xFFE1F1ED),
+                          suffixIcon: Icon(
+                            Icons.visibility_off,
+                            color: Color(0xFFE1F1ED),
+                          ),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12)), borderSide: BorderSide(color: Color(0xFFE1F1ED))),
+                          hintText: 'Придумайте пароль',
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
